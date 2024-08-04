8b05f719a237c24ec4384cfb8d5c4db7a8d0c455
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-04 15:03:21 +0530

    DataTypes and Variables

---
+++ Code_002/Data-Types.py
@@ -0,0 +1,25 @@
+# Data Types and Varables 
+
+#integer
+a = 123
+print(a)
+
+# string
+b = 'B'
+c = '7'
+v = 'I am the Best'
+print(b)
+print(c)
+print(v)
+
+# Boolean
+b1 = True
+print(b1)
+
+# floating point
+f = 16545.52
+print(f)
+d = 5464.2356
+print(d)
+
+
