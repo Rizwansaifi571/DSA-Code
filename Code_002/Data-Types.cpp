35e52b21a9312e0ea52952546f78ee5a87999c55
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-04 15:04:24 +0530

    DataTypes and Variables

---
+++ Code_002/Data-Types.cpp
@@ -0,0 +1,46 @@
+//  Data Types and Variables
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// integer
+    int a = 123;
+    int size = sizeof(a);
+
+    cout << a << endl; 
+    cout << size << endl;
+
+    
+// string with single character
+    char b = 'B';
+    char c = '7';
+
+    cout << b << endl;
+    cout << c << endl;
+    cout << sizeof(b) << endl;
+    cout << sizeof(c) << endl;
+
+
+// boolean
+    bool b1 = true;
+    cout<< b1 << endl;
+    cout << sizeof(b1) << endl;
+
+    
+// float
+    float f = 16545.52;
+    cout << f << endl;
+    cout << sizeof(f) << endl;
+
+
+// double decimal float
+    double d = 5464.2356;
+    cout << d << endl;
+    cout << sizeof(d) << endl;
+
+
+
+
+
