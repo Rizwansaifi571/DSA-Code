06dd224bba94f71f0d9472fec0af61e4c38a8f95
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-05 14:39:05 +0530

    Conditionals and loops

--- Code_003/Conditionals and loops.py
+++ Code_003/Conditionals and loops.py
@@ -30,3 +30,47 @@ else :
     print(f'{a} is less than zero')
 
 
+
+
+# While loop
+
+# Example : Find sum from 1 to n
+
+n = int(input("Enter the value of n  : "))
+i = 1
+sum = 0
+while (i <= n):
+    sum += i
+    i += 1
+print(f'Sum from 1 to {n} is : {sum}')
+
+
+# Example : Find sum of all even number from 1 to n1.
+
+n1 = int(input("Enter the value of n1 : "))
+i = 1
+sum = 0
+while (i <= n1):
+    if (i % 2 == 0):
+        sum += i
+    i += 1
+print (f'Sum of even number from 1 to {n1} is : {sum}')
+
+
+# Example : Find z is prime or not?
+z = int(input("Enter the value of z : "))
+i = 2
+output = True 
+while (i < z):
+    if (z % i == 0):
+        output = False
+        break
+    else :
+        output = True
+    i += 1
+
+if (output == True):
+    print(f'{z} is an prime number')
+else : 
+    print(f'{z} is not an prime number')
+    
