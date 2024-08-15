54fbe530254167e0da2cf639561c25e633460f0e
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-15 16:12:41 +0530

    Bitwise and For Loop

--- Code_005/bitwise_forLoop.py
+++ Code_005/bitwise_forLoop.py
@@ -18,5 +18,32 @@ for i in range (1, n+1):
 print(sum)
 
 
+# Ques: Print fibonacci series.
 
+n = int(input("Enter n : "))
+a = 0
+b = 1
+print (a,b, end = " ")
+for i in range(1, n+1):
+    sum = a + b
+    a = b
+    b = sum
+    print(sum, end = " ")
+
+
+# Ques : Print number is prime or not.
+
+n = int(input("Enter n : "))
+check = 1
+ 
+for i in range (2, n):
+    if (n % i == 0):
+        print(n, "is not a prime number")
+        check = 0
+        break
+    else :
+        check = 1
+
+if check == 1 :
+    print(n, "is a prime number")
         
