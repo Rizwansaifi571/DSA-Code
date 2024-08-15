bb4b857282b1a5602090c31cd89892d44f2bcb93
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-15 16:12:30 +0530

    Bitwise and For Loop

---
+++ Code_005/bitwise_forLoop.py
@@ -0,0 +1,22 @@
+# For Loop 
+
+# Ques. Printing number for 1 to n.
+
+n = int(input("Enter n : "))
+
+for i in range (1, n+1) :
+    print (i)
+
+
+
+# Ques: Print sum for 1 to n.
+
+n = int(input("Enter n : "))
+sum = 0
+for i in range (1, n+1):
+    sum += i
+print(sum)
+
+
+
+        
