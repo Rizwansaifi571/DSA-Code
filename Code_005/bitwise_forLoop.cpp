83f746523fba7eafa846b25bfa5d4d1155662c66
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-15 14:24:29 +0530

    Bitwise and For Loop

---
+++ Code_005/bitwise_forLoop.cpp
@@ -0,0 +1,25 @@
+// Bitwise and for loop 
+
+
+/*BITWISE OPERATOR*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4;
+    int b = 6;
+
+    cout << "a & b : " << (a&b) << endl;
+    cout << "a | b : " << (a|b) << endl;
+    cout << "~ a : " << (~a) << endl;
+    cout << "a ^ b : " << (a^b) << endl;
+    cout << "a << 1 : " << (a<<1) << endl;
+    cout << "a << 2 : " << (a<<2) << endl;  
+    cout << "a >> 1 : " << (a>>1) << endl;
+    cout << "a >> 2 : " << (a>>2) << endl;
+
+
+}
+
+
