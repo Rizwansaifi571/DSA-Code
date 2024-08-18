e2eab0d67c2af893ab43d46018a811bde1fd12a5
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-18 14:23:17 +0530

    Conversion Between Decimal and Binary

--- Code_006/conversion.cpp
+++ Code_006/conversion.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Enter n : ";
+    cout << "Enter decimal number : ";
     cin >> n;
 
     int ans = 0;
@@ -26,4 +26,27 @@ int main() {
 
 cout << "Answer is : " << ans << endl;
 
+
+
+
+
+// Binary to Decimal.
+
+    cout << "Enter binary number : ";
+    cin >> n;
+
+    int i = 0; 
+    ans = 0;
+
+    while(n != 0) {
+        int digit = n % 10;
+
+        if (digit == 1) {
+            ans = ans + pow(2, i);
+        }
+
+        n = n/10;
+        i++;
+    }
+    cout << ans << endl;
 }
