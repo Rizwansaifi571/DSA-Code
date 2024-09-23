590aa998726eee940294659042c59934a55571f2
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-23 22:25:46 +0530

    Array

--- Code_009/array.cpp
+++ Code_009/array.cpp
@@ -15,7 +15,26 @@ void getMinandMax (int arr[], int n) {
     cout << "Maximum element of an array : " << max << endl << endl;
 }
 
-int main() {\
+
+
+
+
+void ReverseArray (int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int first = arr[i];
+        int last = arr[n-i-1];
+        if (i >= n-i-1) {
+            break;
+        } else {
+            arr[n-i-1] = first;
+            arr[i] = last;
+        }
+    }
+}
+
+
+
+int main() {
     cout << "Enter size of an array : " ;
     int arr[100];
     int size;
@@ -26,5 +45,20 @@ int main() {\
     }
 
     getMinandMax(arr, size);
+
+
+    int arr1[5] = {1, 2, 3, 4, 5};
+    ReverseArray(arr1, 5);
+    for (int i = 0; i < 5; i++){
+        cout << arr1[i] << " " ;
+    } cout << endl;
+
+
+    int arr2[6] = {2, 4, 6, 8, 10, 12};
+    ReverseArray(arr2, 6);
+    for (int i = 0; i < 6; i++){
+        cout << arr2[i] << " " ;
+    }
+    
     
     } 
