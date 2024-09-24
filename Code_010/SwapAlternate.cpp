5b3351473b3146cf5783f2e8868a7b805ceb1fdd
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-24 21:41:21 +0530

    SwapAlternate

--- Code_010/SwapAlternate.cpp
+++ Code_010/SwapAlternate.cpp
@@ -14,23 +14,37 @@ void SwapAlternate (int arr[], int size) {
     }
 }
 
+
+void SwapAlternate2 (int arr[], int size) {
+    for(int i = 0; i < size; i += 2) {
+        if (i < size-1) {
+            int first = arr[i];
+            int second = arr[i+1];
+            arr[i] = second;
+            arr[i+1] = first;
+            
+        }    
+    }
+}
+
 void PrintArray (int arr[], int size) {
     for(int i = 0; i < size; i++) {
         cout << arr[i] << " ";
-    }
+    }cout << endl << endl;
 }
 
 int main() {
 
     int array1[5] = {1, 2, 7, 8, 5};
-    SwapAlternate(array1, 5);
+    // SwapAlternate(array1, 5);
+    SwapAlternate2(array1, 5);
     PrintArray(array1, 5);
-    cout << endl << endl;
+
 
 
     int array2[6] = {1, 2, 3, 4, 5, 6};
-    SwapAlternate(array2, 6);
+    // SwapAlternate(array2, 6);
+    SwapAlternate2(array2, 6);
     PrintArray(array2, 6);
-    cout << endl << endl;
 
 }
