bcee8cbe5c5d40460c6bd5de70c55f9d8a1ce0b6
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-11-10 21:19:01 +0530

    Insertion Sorting Using for loop

---
+++ Code_017/InsertionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int arr[],  int size) {
+    for(int i = 1; i < size; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+        for(; j >= 0; j--) {
+            if (arr[j] > temp) {
+                arr[j+1] = arr[j];
+            } else {
+                break;
+            }
+        }
+        arr[j+1] = temp;
+    }
+}
+
+
+void printArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " " ;
+    }
+    cout << endl;
+}
+
+
+int main() {
+    int arr[7] = {10, 1, 7, 4, 8, 2, 11};
+    InsertionSort(arr, 7);
+    printArray(arr, 7);
+
+    return 0;
+}
