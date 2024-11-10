fa9b8d1624ad13630215d2f0b2976655d64a657c
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-11-10 23:01:07 +0530

    Insertion Sorting

--- Code_017/InsertionSort.cpp
+++ Code_017/InsertionSort.cpp
@@ -5,12 +5,9 @@ void InsertionSort(int arr[],  int size) {
     for(int i = 1; i < size; i++) {
         int temp = arr[i];
         int j = i - 1;
-        for(; j >= 0; j--) {
-            if (arr[j] > temp) {
-                arr[j+1] = arr[j];
-            } else {
-                break;
-            }
+        while(j >= 0 && arr[j] > temp) {
+            arr[j+1] = arr[j];
+            j--;
         }
         arr[j+1] = temp;
     }
