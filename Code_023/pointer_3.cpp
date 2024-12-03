8248b7f14a41074eae1558270f641ec19ba61900
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-12-03 22:16:54 +0530

    Pointers

---
+++ Code_023/pointer_3.cpp
@@ -0,0 +1,22 @@
+// Double Pointers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 5;
+    int *ptr = &i;
+    int **ptr2 = &ptr;
+
+    cout << &ptr << endl;
+    cout << ptr2 << endl;
+    
+    cout << ptr << endl;
+    cout << &i << endl;
+
+    cout << i << endl;
+    cout << *ptr << endl;
+    cout << **ptr2 << endl;
+
+}
