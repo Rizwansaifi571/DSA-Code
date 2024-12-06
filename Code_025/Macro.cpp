988e90f3527965b4e4e4d5e7d04b0bf4389356d4
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-12-06 18:38:43 +0530

    Important Keywords

---
+++ Code_025/Macro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.14     // never modifed
+int main() {
+    int r = 5;
+    double area = PI * r * r;
+    cout << "Area of circle is : " << area << endl;
+    return 0;
+}
