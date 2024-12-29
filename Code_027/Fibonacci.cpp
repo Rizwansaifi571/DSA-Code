33ac1e8620af3050cbb7ec4abbe758082c81714a
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-12-29 14:35:50 +0530

    Recursion (Day - 2)

--- Code_027/Fibonacci.cpp
+++ Code_027/Fibonacci.cpp
@@ -3,22 +3,20 @@
 #include <iostream>
 using namespace std;
 
-void fibo(int first, int second, int n) {
-    if (n == 1) {
-        cout << n << "th Fibonacci digit is " << first << endl;
-        return;
-    }
-    if (n == 2) {
-        cout << n << "th Fibonacci digit is " << second << endl;
-        return;
+int fibonacci(int n) {
+
+    if(n == 0) {
+        return 0;
+    } if(n == 1) {
+        return 1;
     }
 
-    fibo(second, first + second, n - 1);
+    int ans = fibonacci(n - 1) + fibonacci(n - 2);
+    return ans;
 }
 
 int main() {
-    int n = 8; // Find the 8th Fibonacci number
-    fibo(0, 1, n);
+    int n = 8;
+    cout << fibonacci(8) << endl;
     return 0;
 }
-
