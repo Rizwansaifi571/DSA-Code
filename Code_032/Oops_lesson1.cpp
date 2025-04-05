5ebce22e0985f96489326c99150444ceffb9fe13
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-04-05 23:10:14 +0530

    Oops Fundamentals

--- Code_032/Oops_lesson1.cpp
+++ Code_032/Oops_lesson1.cpp
@@ -3,14 +3,26 @@ using namespace std;
 
 // class implementation
 class Hero {
+private :
     int health;
+
+public:
+    void getHealth() {
+        cout << health << endl;
+    }
+
+    void setHealth(int h) {
+        health = h;
+    }
 };
 
 int main() {
 
     // object of hero class
-    Hero obj_1;
-    cout << "size of Object : " << sizeof(obj_1) << endl;
+    Hero h1;
+    h1.setHealth(70);
+    h1.getHealth();
+    //cout << "size of Object : " << sizeof(obj_1) << endl;
 
     return 0;
 }
