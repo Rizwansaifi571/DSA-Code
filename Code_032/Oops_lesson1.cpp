38f068c888cbd5123abf989d08d3aa08f0def9f4
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-04-05 22:18:26 +0530

    Hello Class!!

---
+++ Code_032/Oops_lesson1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// class implementation
+class Hero {
+    int health;
+};
+
+int main() {
+
+    // object of hero class
+    Hero obj_1;
+    cout << "size of Object : " << sizeof(obj_1) << endl;
+
+    return 0;
+}
