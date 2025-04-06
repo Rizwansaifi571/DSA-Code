6eda77a3317287b6a4505eb98824a694ee6f1227
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-04-06 13:47:56 +0530

    Oops Fundamentals

--- Code_032/Oops_lesson1.cpp
+++ Code_032/Oops_lesson1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 // class implementation
@@ -20,19 +21,32 @@ public:
 public : 
     int health;
     char level;
+    char *name;
 
     // creating parameterrize constructor
-    Hero(int health, char level) {
+    Hero(int health, char level, char name[]) {
         cout << "parameterized constructor" << endl;
+        this -> name = new char[100];
         this -> health = health;
         this -> level = level;
+        strcpy(this -> name, name);
     }
 
-    Hero(Hero& temp) {
-        cout << "copy constructor" << endl;
-        this -> health = temp.health;
-        this -> level = temp.level;
+    void print() {
+        cout << endl << "[name : " << name << ", ";
+        cout << "Health : " << health << ", ";
+        cout << "Level : " << level << "] ";
+        cout << endl << endl;
+
     }
+
+
+
+    // Hero(Hero& temp) {
+    //     cout << "copy constructor" << endl;
+    //     this -> health = temp.health;
+    //     this -> level = temp.level;
+    // }
 
 };
 
@@ -47,13 +61,32 @@ int main() {
 
 */
 
-    Hero obj_1(85, 'S');
-    cout << obj_1.health << endl;
-    cout << obj_1.level << endl;
+    // Hero obj_1(85, 'S');
+    // cout << obj_1.health << endl;
+    // cout << obj_1.level << endl;
     
-    Hero obj_2(obj_1);
-    cout << obj_2.health << endl;
-    cout << obj_2.level << endl;
+    // Hero obj_2(obj_1);
+    // // Hero obj_2 = obj_1;
+    // cout << obj_2.health << endl;
+    // cout << obj_2.level << endl;
+
+
+    char name[] = "Rizwan";
+    
+    Hero h1(70, 'S', name);
+    
+    Hero h2(h1);
+    
+    h1.print();
+    h2.print();
+
+    h1.name[0] = 'B';
+    cout << "After Update" << endl;
+    h1.print();
+    h2.print();
+
+
+
 
     return 0;
 }
