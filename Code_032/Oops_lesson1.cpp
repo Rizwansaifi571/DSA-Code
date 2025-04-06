e4d3e4c6ea6ed7edb7c760a8a67d4693cce64e1f
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-04-06 12:47:07 +0530

    Oops Fundamentals

--- Code_032/Oops_lesson1.cpp
+++ Code_032/Oops_lesson1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // class implementation
 class Hero {
+/*
 private :
     int health;
 
@@ -14,15 +15,34 @@ public:
     void setHealth(int h) {
         health = h;
     }
+*/
+
+public : 
+    int health;
+    char level;
+
+    // creating parameterrize constructor
+    Hero(int health, char level) {
+        this -> health = health;
+        this -> level = level;
+    }
+
 };
 
 int main() {
 
+/*
     // object of hero class
     Hero h1;
     h1.setHealth(70);
     h1.getHealth();
     //cout << "size of Object : " << sizeof(obj_1) << endl;
+
+*/
+
+    Hero obj_1(85, 'S');
+    cout << obj_1.health << endl;
+    cout << obj_1.level << endl;
 
     return 0;
 }
