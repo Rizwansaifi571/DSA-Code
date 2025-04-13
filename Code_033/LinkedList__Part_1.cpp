768bfe144ea86afbafbaac82b67ac1354baf2105
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-04-13 12:54:58 +0530

    Lineked List Fundamentals

--- Code_033/LinkedList__Part_1.cpp
+++ Code_033/LinkedList__Part_1.cpp
@@ -13,15 +13,40 @@ public:
     }
 };
 
+void intertAtHead(Node* &head, int data) {
+    // create new node
+    Node* temp = new Node(data);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node* &head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " -> ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
 int main() {
     // dyanmically
     Node* node1 = new Node(99);
-    cout << node1 -> data << endl;
-    cout << node1 -> next << endl;
+    // cout << node1 -> data << endl;
+    // cout << node1 -> next << endl;
 
     // statical
     // Node node1(99);
     // cout << node1.data << endl;
     // cout << node1.next << endl;
+
+
+    // head creation
+    Node* head = node1;
+
+    intertAtHead(head, 109);
+    intertAtHead(head, 209);
+    intertAtHead(head, 309);
+    print(head);
     return 0;
 }
