3c527872d233e2390b63c1cb647dc4fe29524313
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-04-13 14:16:11 +0530

    Lineked List Fundamentals

--- Code_033/LinkedList__Part_1.cpp
+++ Code_033/LinkedList__Part_1.cpp
@@ -13,11 +13,43 @@ public:
     }
 };
 
-void intertAtHead(Node* &head, int data) {
+void insertAtHead(Node* &head, int data) {
     // create new node
     Node* temp = new Node(data);
     temp -> next = head;
     head = temp;
+}
+
+void insertAtTail(Node* &tail, int data) {
+    Node* temp = new Node(data);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void insertAtPosition(int pos, int data, Node* &head, Node* & tail) {
+
+    if(pos == 1) {
+        insertAtHead(head, data);
+        return;
+    }
+
+    Node* temp = head;
+    int count = 1;
+    
+    while(count < pos - 1) {
+        temp = temp -> next;
+        count++;
+    }
+
+    if(temp -> next == NULL) {
+        insertAtTail(tail, data);
+        return;
+    }
+    
+    
+    Node* nodetoInsert = new Node(data);
+    nodetoInsert -> next = temp -> next;
+    temp -> next = nodetoInsert;
 }
 
 void print(Node* &head) {
@@ -31,7 +63,7 @@ void print(Node* &head) {
 
 int main() {
     // dyanmically
-    Node* node1 = new Node(99);
+    Node* node1 = new Node(1);
     // cout << node1 -> data << endl;
     // cout << node1 -> next << endl;
 
@@ -44,9 +76,18 @@ int main() {
     // head creation
     Node* head = node1;
 
-    intertAtHead(head, 109);
-    intertAtHead(head, 209);
-    intertAtHead(head, 309);
+    // tail creation 
+    Node* tail = node1;
+
+
+    insertAtHead(head, 2);
+    insertAtHead(head, 3);
+    insertAtHead(head, 4);
+    insertAtTail(tail, 5);
+    insertAtPosition(1, 6, head, tail);
+    insertAtPosition(5, 7, head, tail);
     print(head);
+    cout << "head " << head -> data << endl;
+    cout << "tail " << tail -> data << endl;
     return 0;
 }
