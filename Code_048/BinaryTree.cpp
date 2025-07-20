ba7c6e017b684b2b8685b7eafcf2cfa6577d8580
RizwanSaifi571 <rizwansaifi2614@gmail.com>
2025-07-20 13:38:31 +0530

    Binary Tree and It's Representation

--- Code_048/BinaryTree.cpp
+++ Code_048/BinaryTree.cpp
@@ -101,10 +101,45 @@ void ReverselevelOrderTraversal(node* root) {
     }
 }
 
+
+void buildFromLevelOrder(node* &root) {
+    queue<node*> q;
+    cout << "Enter Root Data : ";
+    int data;
+    cin >> data;
+    root = new node(data);
+    q.push(root);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        cout << "Enter left Node for {" << temp->data << "} : ";
+        int leftData;
+        cin >> leftData; 
+
+        if(leftData != -1) {
+            temp -> left = new node(leftData);
+            q.push(temp -> left);
+        }
+
+
+        cout << "Enter Right Node for {" << temp->data << "} : ";
+        int RightData;
+        cin >> RightData; 
+
+        if(RightData != -1) {
+            temp -> right = new node(RightData);
+            q.push(temp -> right);
+        }
+    }
+}
+
 int main() {
 
     node* root = NULL;
 
+    /*
     buildTree(root);
     // 1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
 
@@ -116,5 +151,17 @@ int main() {
     cout << endl;
     cout << endl;
     ReverselevelOrderTraversal(root);
+    */
+
+    buildFromLevelOrder(root);
+    // 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
+
+
+    cout << endl;
+    cout << endl;
+    levelOrderTraversal(root);
+
+
+
     return 0;
 }
